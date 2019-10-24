8d79851c277247b5c6f69260460b932a08ab04e2
vizeke <1816558+vizeke@users.noreply.github.com>
2019-10-24 19:24:30 -0300

    feat(app): mvp

--- lib/main.dart
+++ lib/main.dart
@@ -2,35 +2,40 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// You can read about packages here: https://flutter.io/using-packages/
 import 'package:flutter/material.dart';
 
-class HelloRectangle extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        color: Colors.greenAccent,
-        height: 400.0,
-        width: 300.0,
-        child: Center(
-          child: Text(
-            'Hello!',
-            style: TextStyle(fontSize: 40.0),
-          ),
-        ),
-      ),
-    );
-  }
+// You can use a relative import, i.e. `import 'category_route.dart;'` or
+// a package import.
+// More details at http://dart-lang.github.io/linter/lints/avoid_relative_lib_imports.html
+import 'category_route.dart';
+
+/// The function that is called when main.dart is run.
+void main() {
+  runApp(UnitConverterApp());
 }
 
-void main() {
-  runApp(
-    MaterialApp(
+/// This widget is the root of our application.
+///
+/// The first screen we see is a list [Categories], each of which
+/// has a list of [Unit]s.
+class UnitConverterApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(title: Text("Hello Rectangle")),
-        body: HelloRectangle(),
+      title: 'Unit Converter',
+      theme: ThemeData(
+        fontFamily: 'Raleway',
+        textTheme: Theme.of(context).textTheme.apply(
+              bodyColor: Colors.black,
+              displayColor: Colors.grey[600],
+            ),
+        // This colors the [InputOutlineBorder] when it is selected
+        primaryColor: Colors.grey[500],
+        textSelectionHandleColor: Colors.green[500],
       ),
-    ),
-  );
+      home: CategoryRoute(),
+    );
+  }
 }
